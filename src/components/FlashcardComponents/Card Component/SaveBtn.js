4f456b9f084f1f82fbe076f363c60e3064f66497
cjlaserna <49669196+cjlaserna@users.noreply.github.com>
2022-04-19 15:02:01 -0700

    add to gitignore

--- src/components/FlashcardComponents/Card Component/SaveBtn.js
+++ src/components/FlashcardComponents/Card Component/SaveBtn.js
@@ -37,7 +37,7 @@ export const SaveBtn = ({ card, setName, setCreator, user }) => {
 
 	async function starCard() {
 		// if there are cards starred, fetch cards, add to it, send back new cards
-		if (starred.length() !== 0) {
+		if (starred !== []) {
 			// check if the card is already starred
 			const isCardInSet = checkIfStarred();
 
